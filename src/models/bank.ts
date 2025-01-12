0a2c6d20901aff4e69d353894f130d0f21c6511d
Pratiyush Kumar <pratiyush.inv@gmail.com>
2025-01-12 13:57:16 +0530

    send functionality

--- src/models/bank.ts
+++ src/models/bank.ts
@@ -1,20 +1,28 @@
-import BankAccount from "./bank-account";
+import BankAccount from './bank-account';
 
 export default class Bank {
-    constructor(){
-        let bankId;
-    }
+  constructor() {
+    let bankId;
+  }
 
-    static create(options?: {isNegativeAllowed: boolean}): Bank{
-        return new Bank()
-    }
+  static create(options?: { isNegativeAllowed: boolean }): Bank {
+    return new Bank();
+  }
 
-    getId(){
-        return 1;
-    }
+  getId() {
+    return 1;
+  }
 
-    
-    createAccount(initialBalance: number): BankAccount {
-        return new BankAccount(initialBalance);
-    }
+  createAccount(initialBalance: number): BankAccount {
+    return new BankAccount(initialBalance);
+  }
+
+  send(
+    senderUserId: string,
+    receiverUserId: string,
+    amount: number,
+    bankId?: number
+  ): void {
+    // Implementation of the send method
+  }
 }
